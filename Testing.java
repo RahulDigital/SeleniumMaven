839e93a34e4922a9513c31c7ccbe0909f23e4abe
Rahul Lanjewar <rahul.lanjewar.qa@gmail.com>
2015-08-31 02:06:43 +0530

    1 Commit with change by adding line 2

--- Testing.java
+++ Testing.java
@@ -4,6 +4,8 @@ public class Testing {
 
 	public static void main(String[] args) {
 		System.out.println("My first line from eclipse");
+		System.out.println("My 2nd line line from eclipse. taking update without commit");
+		// SVN repo is not updated and local chnages remains as is. uncommited
 
 	}
 
