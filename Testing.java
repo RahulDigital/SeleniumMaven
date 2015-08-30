a4ac3ce78cd524d99f48092d5a81d1170979d18f
Rahul Lanjewar <rahul.lanjewar.qa@gmail.com>
2015-08-31 03:04:57 +0530

    final

--- Testing.java
+++ Testing.java
@@ -7,9 +7,8 @@ public class Testing {
 		System.out.println("My 2nd line line from eclipse. taking update without commit");
 		// SVN repo is not updated and local chnages remains as is. uncommited
 		System.out.println("My 3rd line by other user when ohter user offline. and then taking update");
-		
 		System.out.println("My 5th line by user 2 when other user 1 also online. and then taking update");
-
+		System.out.println("My 4th line by user 1 when other user alos online. and then taking update");
 	}
 
 }
