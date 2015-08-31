465d2df94469669ca44a246afa3fb2d6ba5b28b6
Rahul Lanjewar <rahul.lanjewar.qa@gmail.com>
2015-09-01 02:16:53 +0530

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -3,18 +3,7 @@ package test;
 public class Testing {
 
 	public static void main(String[] args) {
-		System.out.println("My first line from eclipse");
-		System.out.println("My 2nd line line from eclipse. taking update without commit");
-		// SVN repo is not updated and local chnages remains as is. uncommited
-		System.out.println("My 3rd line by other user when ohter user offline. and then taking update");
-		System.out.println("My 5th line by user 2 when other user 1 also online. and then taking update");
-		System.out.println("My 4th line by user 1 when other user alos online. and then taking update");
-		System.out.println("Same ling change by user 1 when other user alos online. and then taking update");
-		System.out.println("Same ling change by user 2 when other user alos online. and then taking update");
-		sds New changfe before commit
-		User 2 make changes and Commit - user 1 is online ( on diff lines)
-	User 1 Commit agin - take
-	User 1 make changes and Commit - user 2 is online 
+		
 	}
 
 }
