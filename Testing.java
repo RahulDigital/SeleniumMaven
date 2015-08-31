0214fa4610041f985c3ee53edaada28dad77b045
Rahul Lanjewar <rahul.lanjewar.qa@gmail.com>
2015-09-01 02:41:59 +0530

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -3,7 +3,6 @@ package test;
 public class Testing {
 
 	public static void main(String[] args) {
-		user 1 changes line 1
-	}user 2 changes line 2 - Same line changes by User2
-	}user 2 changes line 2 - Same line changes by User 1 - again
+	
+	}
 }
